51947e2c06f56e89f4334d1240012105b611f661
Said Gonzalez <alfredjr.gonzalez.19@gmail.com>
2025-05-31 23:08:16 -0600

    Piezas V1

---
+++ Codigo/piezas.h
@@ -0,0 +1,14 @@
+#ifndef PIEZAS_H
+#define PIEZAS_H
+//definir las constantes del color de las piezas
+#define BLANCO 0
+#define NEGRO 1
+//Definir las constantes de las piezas
+#define PEON 0
+#define TORRE 1
+#define CABALLO 2
+#define ALFIL 3
+#define DAMA 4
+#define REY 5
+
+#endif // PIEZAS_H
