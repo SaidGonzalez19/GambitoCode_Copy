16dc348a83e9374e7c1a3b713187c57b783a74c6
Said Gonzalez <alfredjr.gonzalez.19@gmail.com>
2025-05-31 23:27:48 -0600

    Terminado el piezas.c y piezas.h

--- Codigo/piezas.h
+++ Codigo/piezas.h
@@ -10,5 +10,10 @@
 #define ALFIL 3
 #define DAMA 4
 #define REY 5
-
+//funciones
+int es_pieza_blanca(char pieza);
+int es_pieza_negra(char pieza);
+int obtener_tipo_pieza(char pieza);
+int es_casilla_vacia(char pieza);
+int son_del_mismo_color(char pieza1, char pieza2);
 #endif // PIEZAS_H
